382175203918ebbcf4dab01de510f089e4b530e0
N0ksa <leonsarko7@gmail.com>
2023-02-24 14:45:25 +0100

    Create Problem 1. Find the sum of all the multiples of 3 or 5 below 1000.cpp

---
+++ Problem 1. Find the sum of all the multiples of 3 or 5 below 1000.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+int main()
+{
+
+	unsigned int a = 3;
+	unsigned int b = 5;
+	unsigned int sum = 0;
+
+	for (int i = 1; i < 1000; ++i)
+	{
+		if (i % a == 0 || i % b == 0)
+			sum += i;
+	}
+
+	std::cout << "Sum is: " << sum << std::endl;
+
+
+
+	return 0;
+}
