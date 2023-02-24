d333dd7131d03f42b99547a6496bcb023276b9f9
N0ksa <leonsarko7@gmail.com>
2023-02-24 21:53:15 +0100

    Upload

---
+++ Problem 3. Largest prime factor.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+
+//The prime factors of 13195 are 5, 7, 13 and 29.
+//What is the largest prime factor of the number 600851475143 ?
+
+
+
+unsigned long int getMaxPrime(unsigned long long int n)
+{
+	unsigned long maxPrime;
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n /= 2;
+	}
+	for (unsigned long int i = 3; i < sqrt(n); i += 2)
+	{
+		if (n % i == 0)
+		{
+			maxPrime = i;
+			n /= i;
+		}
+	}
+	if (n > 2)
+		maxPrime = n;
+	return maxPrime;
+}
+
+int main()
+{
+	unsigned long long int n = 600851475143;
+	std::cout << "The largest prime factor of the number 600851475143 is : " << getMaxPrime(n);
+	return 0;
+}
---
+++ Problem 4. Largest palindrome product.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+
+//A palindromic number reads the same both ways.The largest palindrome made from the product of two 2 - digit numbers is 9009 = 91 × 99.
+//Find the largest palindrome made from the product of two 3 - digit numbers.
+
+bool isPalindrome(unsigned long int n)
+{
+	unsigned long int reverse = 0;
+	int remainder = 0;
+	unsigned long int m = n;
+
+	while (n != 0)
+	{	
+		remainder = n % 10;
+		reverse = reverse * 10 + remainder;
+		n /= 10;
+	}
+
+	if (reverse == m)
+		return true;
+	return false;
+}
+
+
+int main()
+{
+	unsigned int largestP = 0;
+	unsigned int product = 0;
+
+	for (int i = 999; i != 0; --i)
+		for (int j = 999; j != 0; --j)
+		{
+			if (isPalindrome (i * j))
+			{
+				product = i * j;
+				if (largestP < product)
+					largestP = product;
+			}
+		}
+
+
+	std::cout << "The largest palindrome made from the product of two 3 - digit numbers is : " << largestP << std::endl;
+
+
+	return 0;
+}
